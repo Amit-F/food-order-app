018e2e325ac6bfb05b7e0a37a12339f753cd3d37
Amit Fink <amitfink@gmail.com>
2026-02-19 21:02:50 +0200

    finished related products logic, fixed identical related product bug, and started on shopping cart

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title';
 
 const Cart = () => {
+
+  const { products, currency, cartItems} = useContext(ShopContext);
+
+  const [cartData,setCartData] = useState([]);
+
+  useEffect(()=>{
+
+    const tempData = [];
+    for(const items in cartItems){
+      for(const item in cartItems[items]){
+        if (cartItems[items][item] > 0){
+          tempData.push({
+            _id: items,
+            servingAmount: item,
+            quantity: cartItems[items][item]
+          })
+        }
+      }
+    }
+    setCartData(tempData);
+    
+  },[cartItems])
+
+
+
+
   return (
-    <div>Cart</div>
+    <div className='border-t pt-14'>
+      
+      <div className='text-2xl mb-3'>
+        <Title text1={'YOUR'} text2={'CART'}/>
+      </div>
+
+      <div >
+        {
+          cartData.map((item,index)=>{
+            const productData = products.find((product)=> product._id === item._id);
+
+            return (
+              <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+                <div className='flex items-start gap-6'>
+                  <img src={productData.image[0]} className='w-16 sm:w-20' alt="" />
+                  <div>
+                    <p className='text-xs sm:text-lg font-m'>{productData.name}</p>
+                    <div className='flex items-center gap-5 mt-2'>
+                      <p>Price Per Serving: {currency}{productData.price}</p>
+                      <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>Number of Servings: {item.servingAmount}</p>
+                      <p>Quantity: </p>
+                      <input className='border max-w-10 sm:mx-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity}/>
+                      {/* <p>Total Item Price: {currency}{productData.price * item.servingAmount}</p> */}
+                    </div>
+                  </div>
+                </div>
+                <p>Total Item Price: {currency}{productData.price * item.servingAmount * item.quantity}</p>
+                {/* <input className='border max-w-10 sm:mx-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity}/> */}
+              </div>
+            )
+          })
+        }
+      </div>
+
+    </div>
   )
 }
 
